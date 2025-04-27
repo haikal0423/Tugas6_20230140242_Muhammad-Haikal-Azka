db8219084adc6fbef9744e71dfd114906710ea8d
haikal0423 <mhaikal23azka@gmail.com>
2025-04-27 15:53:15 +0700

    HTML Multimedia

---
+++ jsCH5.js
@@ -0,0 +1,15 @@
+const video = document.getElementById('video');  
+const audio = document.getElementById('audio');  
+const gambar = document.getElementById('gambar');  
+
+video.addEventListener("click", function () {  
+    alert("Ini video Pemandangan Alam.");  
+});  
+
+audio.addEventListener("click", function () {  
+    alert("Ini audio Suara Tradisional.");  
+});  
+
+gambar.addEventListener("click", function () {  
+    alert("Ini gambar Tarian Tradisional.");  
+});  
